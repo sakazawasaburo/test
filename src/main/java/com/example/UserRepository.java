d3133dd3e349f204f5e448f056117e71a4a3673d
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-17 11:24:27 +0900

    l

--- src/main/java/com/example/UserRepository.java
+++ src/main/java/com/example/UserRepository.java
@@ -9,7 +9,7 @@ import com.example.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	/*
+
 	public User findByCustid(String custid);
 
     public User findByUsername(String username);
@@ -17,7 +17,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     public User findByOrgname(String orgname);
 
     public User findByPassword(String password);
-	*/
+
 
 
 }
