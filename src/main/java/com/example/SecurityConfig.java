84b0cba15a6b328d391bcd753bb37b714e5baf2d
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-18 14:34:03 +0900

    k

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -48,8 +48,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Value("${spring.datasource.url}")
 	private String dbUrl;
 
-	@Autowired
-	private userService userservice;
+	//@Autowired
+	//private userService userservice;
 
 	@Autowired
 	@Qualifier("dataSource")
