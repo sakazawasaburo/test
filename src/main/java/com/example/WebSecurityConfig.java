0652f79679da1fd25e1ae96c1235ce59a998daeb
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-18 10:55:48 +0900

    a

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -1,79 +1,14 @@
-/*package com.example.config;
+/*package com.example;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
-
-@Controller
-@SpringBootApplication
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Value("${spring.datasource.url}")
-	private String dbUrl;
-
-	@Autowired
-	@Qualifier("dataSource")
-	private DataSource dataSource;
-
-	private static final String ACCOUNT_QUERY = "INSERT INTO userdata(custid,username,orgname,password) VALUES(,,,)";
-
-
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-	    auth.jdbcAuthentication()
-		.dataSource(dataSource)
-		.usersByUsernameQuery(ACCOUNT_QUERY)
-	    .passwordEncoder(passwordEncoder());
-	}
-
-
-	@Bean
-	public DataSource dataSource() throws SQLException {
-		if (dbUrl == null || dbUrl.isEmpty()) {
-			return new HikariDataSource();
-		} else {
-			HikariConfig config = new HikariConfig();
-			config.setJdbcUrl(dbUrl);
-			return new HikariDataSource(config);
-		}
-	}
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
-	}
-
-
-}
+import com.example.User;
+import com.example.userService;
 */
