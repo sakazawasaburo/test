1772c855b313c2bd0ca5385c46c5014a3c58f4f7
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-18 16:52:45 +0900

    a

--- src/main/java/com/example/UserRepository.java
+++ src/main/java/com/example/UserRepository.java
@@ -2,9 +2,11 @@ package com.example;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import com.example.User;
 
 
 @Repository
+@Component
 public interface UserRepository extends JpaRepository<User, String> {
 }
--- src/main/java/com/example/userService.java
+++ src/main/java/com/example/userService.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Component;
 
 @Service
+@Component
 public class userService  {
 
     @Autowired
