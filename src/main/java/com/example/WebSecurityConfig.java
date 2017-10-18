da35d2ed8013862652972c9ddfa7a586245ccbab
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-18 16:36:46 +0900

    a

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-/*package com.example;
+package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +8,46 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.User;
 import com.example.userService;
-*/
+
+
+@SpringBootApplication
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Autowired
+	@Qualifier("dataSource")
+	private DataSource dataSource;
+
+	@Autowired
+    private userService userservice;
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            	.antMatchers("/signup").hasAuthority("ADMIN")
+
+    }
+
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .userDetailsService(userService)
+            .passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
