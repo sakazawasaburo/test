c786416e9fe139d7b9af26f1537439ee28da617c
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-19 16:52:30 +0900

    a

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -46,7 +46,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Main.class, args);
 	}
-/*
+
 	//@Autowired
 	private userService userservice;
 
@@ -72,7 +72,7 @@ public class Main {
 
 	        return "signup";
 	    }
-*/
+
 
 	@RequestMapping({"/","/login"})
 	String login() {
