8275baa801dbf2987568152595af06beef66472b
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-19 17:53:55 +0900

    a

--- src/main/java/com/example/userService.java
+++ src/main/java/com/example/userService.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-//import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import com.example.UserRepository;
 
 @Service
 @Component
