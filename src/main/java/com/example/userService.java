6476583d570368fbf84897a04c5697ec3d2c7b72
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-20 11:15:19 +0900

    a

--- src/main/java/com/example/userService.java
+++ src/main/java/com/example/userService.java
@@ -97,7 +97,7 @@ public class userService{
     //public void registerUser(String custid,String username,String orgname,String password) {
         //User user = new User(custid,username, orgname, passwordEncoder.encode(password));
         //repository.save(user);
-    @RequestMapping("/db")
+    @RequestMapping("/signup")
         String db(Map<String, Object> model){
 	        try (Connection connection = dataSource.getConnection()) {
 	  	      Statement stmt = connection.createStatement();
