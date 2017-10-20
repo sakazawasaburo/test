59a503e6947b9a519024db53280016441eea8a61
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-20 09:03:48 +0900

    k

--- src/main/java/com/example/userService.java
+++ src/main/java/com/example/userService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Component;
 
-import com.example.UserRepository;
+//import com.example.UserRepository;
 import com.example.User;
 
 @Service
