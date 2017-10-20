2cedc994f063f3a3a2e682bb35f485d9a75c8eef
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-20 09:16:17 +0900

    k

--- src/main/java/com/example/userService.java
+++ src/main/java/com/example/userService.java
@@ -27,6 +27,27 @@ public class userService  {
     public void registerUser(String custid,String username,String orgname,String password) {
         User user = new User(custid,username, orgname, passwordEncoder.encode(password));
         //repository.save(user);
+        String db(Map<String, Object> model) {
+        try (Connection connection = dataSource.getConnection()) {
+  	      Statement stmt = connection.createStatement();
+  	      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+  	      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+  	      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+  	      ArrayList<String> output = new ArrayList<String>();
+  	      while (rs.next()) {
+  	        output.add("Read from DB: " + rs.getTimestamp("tick"));
+  	      }
+
+  	      model.put("records", output);
+  	      return "db";
+  	    } catch (Exception e) {
+  	      model.put("message", e.getMessage());
+  	      return "error";
+  	    }
+
+
+
     }
 
 }
