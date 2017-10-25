622b26595eb19c28fd703a25237118907fc71d35
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-25 11:49:08 +0900

    a

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -43,13 +43,9 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 @Controller
 @SpringBootApplication
