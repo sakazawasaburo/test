7f9c557c8fccb4c3b2b7d6868d7d71c06fa1d212
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-10-31 10:07:00 +0900

    a

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
+            //.userDetailsService(userService)
             .passwordEncoder(passwordEncoder())
         	.dataSource(dataSource);
     }
